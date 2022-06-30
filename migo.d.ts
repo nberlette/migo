e48ad067c75059dae6f19cbe4e055daaf01b5bd8
Nicholas Berlette <nick@berlette.com>
2022-06-30 14:40:19 -0700

    ✨ add migo.d.ts type definitions file

---
+++ migo.d.ts
@@ -0,0 +1,4 @@
+type ConnInfo = Awaitable<import("sift").ConnInfo>;
+type Routes = Awaitable<import("sift").Routes>;
+type PathParams = Awaitable<import("sift").PathParams>;
+type HeadingLevel = Lowercase<`h${1 | 2 | 3 | 4 | 5 | 6}`>;
