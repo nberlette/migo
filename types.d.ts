7a63441fdd09990fa5beca408c767cd72042f4e1
Nicholas Berlette <nick@berlette.com>
2022-11-29 12:13:08 -0800

    types: update type definitions

--- types.d.ts
+++ types.d.ts
@@ -23,6 +23,9 @@ type Id<U, Optional = true> =
     ? { -readonly [K in keyof T]: Id<T[K], Optional> }
   : U;
 
+type Maybe<T> = T | null | undefined;
+type MaybeArray<T> = Maybe<T> | Maybe<T>[];
+
 declare namespace Props {
   type ViewBox = `${number} ${number} ${number} ${number}`;
   type RemoteURL = `http${"s" | ""}://${string}`;
@@ -35,19 +38,17 @@ declare namespace Props {
   type StrokeLinecap = "butt" | "round" | "square";
   type StrokeLinejoin = "miter" | "round" | "bevel";
 
-  type SVG = {
-    xmlns: Union<"http://www.w3.org/2000/svg">;
+  type SVG = Partial<{
+    xmlns: "http://www.w3.org/2000/svg";
     viewBox: ViewBox;
     width: number | `${number}`;
     height: number | `${number}`;
     pxRatio: number | `${number}`;
     borderRadius: number | `${number}`;
-    role: Union<"img">;
-    ariaLabel: string;
     bgColor: string;
     title: string;
     subtitle: string;
-  };
+  }>;
 
   type Text = {
     x: number | `${number}`;
@@ -72,39 +73,45 @@ declare namespace Props {
 
   // love these mappable types from TS 4.x 😍
   // props for the subtitle component
-  type Title = Id<{ [K in keyof Text as `title${Capitalize<K>}`]: Text[K] }>;
+  type Title = Partial<
+    {
+      [K in keyof Text as `title${Capitalize<K>}`]: Text[K];
+    }
+  >;
 
   // props for the subtitle component
-  type Subtitle = Id<{ [K in keyof Title as `sub${K}`]: Title[K] }>;
+  type Subtitle = Partial<
+    {
+      [K in keyof Title as `sub${K}`]: Title[K];
+    }
+  >;
 
+  type StrokesNShadows = Extract<keyof Text, `${"stroke" | "shadow"}${string}`>;
   // deno-fmt-ignore
-  type Icon = Id<
+  type Icon = Partial<
     & { 
-        [K in Extract<keyof Text, `${"stroke" | "shadow"}${string}`> as `icon${
-          Capitalize<K>
-        }`]: Text[K];
+        [K in StrokesNShadows as `icon${Capitalize<K>}`]: Text[K];
       }
-    & { [K in "w" | "h" | "x" | "y" as `icon${K}`]: number | `${number}`; }
+    & { [K in `icon${"W" | "H" | "X" | "Y"}`]: number | `${number}`; }
     & {
       icon: boolean | string;
       iconUrl: RemoteURL | DataURL;
       iconColor: string;
-      iconStroke: string;
       viewBox: ViewBox;
     }
   >;
 }
 
-interface TitleProps extends Props.Title {
+interface SVGProps extends Partial<Props.SVG> {
   /* inherit */
 }
-interface SubtitleProps extends Props.Subtitle {
+interface TitleProps extends Partial<Props.Title> {
   /* inherit */
 }
-interface IconProps extends Props.Icon {
+interface SubtitleProps extends Partial<Props.Subtitle> {
   /* inherit */
 }
-interface SVGProps extends Props.SVG {
+interface IconProps extends Partial<Props.Icon> {
   /* inherit */
 }
 interface AllProps extends TitleProps, SubtitleProps, IconProps, SVGProps {
@@ -112,41 +119,43 @@ interface AllProps extends TitleProps, SubtitleProps, IconProps, SVGProps {
 }
 
 declare namespace Meta {
-  type SEO = Id<OpenGraph & Twitter & Generic>;
+  type SEO = Partial<OpenGraph & Twitter & Generic & Layout>;
   type OpenGraph = {
-    "og:image"?: string;
-    "og:url"?: string;
-    "og:type"?: string;
-    "og:title"?: string;
-    "og:description"?: string;
-    "og:author"?: string;
-    "og:image:width"?: string;
-    "og:image:height"?: string;
-    "og:image:alt"?: string;
-    "og:image:src"?: string;
+    "og:image": string;
+    "og:url": string;
+    "og:type": string;
+    "og:title": string;
+    "og:description": string;
+    "og:author": string;
+    "og:image:width": string;
+    "og:image:height": string;
+    "og:image:alt": string;
+    "og:image:src": string;
   };
   type Twitter = {
-    "twitter:card"?: string;
-    "twitter:creator"?: string;
-    "twitter:image"?: string;
-    "twitter:site"?: string;
-    "twitter:summary"?: string;
-    "twitter:title"?: string;
-    "twitter:description"?: string;
+    "twitter:card": string;
+    "twitter:creator": string;
+    "twitter:image": string;
+    "twitter:site": string;
+    "twitter:summary": string;
+    "twitter:title": string;
+    "twitter:description": string;
   };
   type Generic = {
-    title?: string;
-    author?: string;
-    description?: string;
-    keywords?: string;
+    title: string;
+    author: string;
+    description: string;
+    keywords: string;
   };
   type Layout = {
-    viewport?: string;
-    canonical?: string;
-    favicon?: string;
-    "mask-icon"?: string;
-    "theme-color"?: string;
+    viewport: string;
+    canonical: string;
+    favicon: string;
+    "mask-icon": string;
+    "theme-color": string;
   };
 }
 
-interface Meta extends Meta.Layout, Meta.SEO {}
+interface Meta extends Meta.SEO {
+  /* inherit */
+}
